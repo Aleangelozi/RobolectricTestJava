3ee14d92c38eb41a16e8c7978e6d23e247af12e3
aleangelozi <aleangelozi@gmail.com>
2021-07-04 15:07:13 -0300

    Adding new unit tests

--- app/src/test/java/com/aleangelozi/robolectrictestjava/WelcomeActivityTest.java
+++ app/src/test/java/com/aleangelozi/robolectrictestjava/WelcomeActivityTest.java
@@ -12,23 +12,37 @@ import org.robolectric.RobolectricTestRunner;
 import org.robolectric.RuntimeEnvironment;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 import static org.robolectric.Shadows.shadowOf;
 
 
 @RunWith(RobolectricTestRunner.class)
 public class WelcomeActivityTest {
-    private WelcomeActivity activity;
+    private WelcomeActivity welcomeActivity;
+    private LoginActivity loginActivity;
 
     @Before
     public void setup() {
-        activity = Robolectric.setupActivity(WelcomeActivity.class);
+        welcomeActivity = Robolectric.setupActivity(WelcomeActivity.class);
+        loginActivity = Robolectric.setupActivity(LoginActivity.class);
+    }
+
+    @Test
+    public void welcomeActivity_ShouldNotBeNull() {
+        assertNotNull(welcomeActivity);
+        welcomeActivity.finish();
+    }
+
+    @Test
+    public void loginActivity_ShouldNotBeNull() {
+        assertNotNull(loginActivity);
     }
 
     @Test
     public void clickingLogin_shouldStartLoginActivity() {
-        activity.findViewById(R.id.login).performClick();
+        welcomeActivity.findViewById(R.id.login).performClick();
 
-        Intent expectedIntent = new Intent(activity, LoginActivity.class);
+        Intent expectedIntent = new Intent(welcomeActivity, LoginActivity.class);
         Intent actual = shadowOf(RuntimeEnvironment.application).getNextStartedActivity();
         assertEquals(expectedIntent.getComponent(), actual.getComponent());
     }
